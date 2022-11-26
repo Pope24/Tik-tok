49c98da4a455d5223732b016e673495bd2206c3e
Pope24 <110238804+Pope24@users.noreply.github.com>
2022-11-26 20:36:25 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import { Fragment } from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
 import { publicRoute, privateRoute } from '~/pages/route';
+import { DefaultLayout } from './component/Layout';
 function App() {
     return (
         <div className="App">
             <Routes>
                 {publicRoute.map((route, index) => {
+                    const Layout = route.layout === null ? Fragment : DefaultLayout;
                     const Page = route.component;
-                    return <Route key={index} path={route.path} element={<Page />}></Route>;
+                    return (
+                        <Route
+                            key={index}
+                            path={route.path}
+                            element={
+                                <Layout>
+                                    <Page />
+                                </Layout>
+                            }
+                        ></Route>
+                    );
                 })}
             </Routes>
         </div>
--- src/pages/route/index.js
+++ src/pages/route/index.js
@@ -1,8 +1,12 @@
 import HomePage from '~/pages/Home';
 import FollowingPage from '~/pages/Following';
+import ProfilePage from '~/pages/Profile';
+import UploadPage from '~/pages/Upload';
 const publicRoute = [
     { path: '/', component: HomePage },
     { path: '/following', component: FollowingPage },
+    { path: '/profile', component: ProfilePage },
+    { path: '/upload', component: UploadPage, layout: null },
 ];
 const privateRoute = [];
 export { publicRoute, privateRoute };
